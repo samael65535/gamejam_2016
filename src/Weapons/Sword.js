d9843ac3e5afd82b697faa4e7d1b6a0de829244e
samael <samael.65535@gmail.com>
2016-01-31 16:07:36 +0800

    同归

--- src/Weapons/Sword.js
+++ src/Weapons/Sword.js
@@ -41,20 +41,26 @@ var Sword = cc.Sprite.extend({
     },
 
     checkDamage: function(playerArray) {
+        var arr = [];
         _.each(playerArray, function(p, k){
             if(this._owner == null || this._owner.playerNum == p.playerNum || p.isDeath) return;
             var rectW1 = this.getBoundingBoxToWorld();
             var rectW2 = p._weapon.getBoundingBoxToWorld();
             var rect2 = p.getBoundingBoxToWorld();
-
+            var rect1 = this._owner.getBoundingBoxToWorld();
             if (cc.rectIntersectsRect(rectW1, rectW2)) {
                 p.pushBack();
                 this._owner.pushBack();
-            } else if (cc.rectIntersectsRect(rectW1, rect2) && !p.isDeath) {
+            }
+            if (cc.rectIntersectsRect(rectW1, rect2) && !p.isDeath) {
                 var pos1 = cc.p(rectW1.x, rectW1.y);
                 var pos2 = cc.p(rect2.x, rect2.y);
-
-                p.doDeath(Util.faceType(pos1,  pos2));
+                p.doDeath(Util.faceType(pos1, pos2));
+            }
+            if (cc.rectIntersectsRect(rectW2, rect1) && !this._owner.isDeath) {
+                var pos3 = cc.p(rectW2.x, rectW2.y);
+                var pos4 = cc.p(rect1.x, rect1.y);
+                this._owner.doDeath(Util.faceType(pos3,  pos4));
             }
         }, this);
     }
